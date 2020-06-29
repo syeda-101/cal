35a2a30e2afafccf73c6cff9533747f4fb89102e
Ahreem Syed <67508293+syeda-101@users.noreply.github.com>
2020-06-29 16:16:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-function calcNumbers(result){
-        form.displayResult.value=form.displayResult.value+result;
-        
-    }
+var myWindow;
+function ans(num) {
+    var input = document.getElementById('in');
+    input.value += num;
+
+
+}
+
+function eva() {
+    var input = document.getElementById('in');
+    input.value = eval(input.value)
+
+
+    num = eva()
+}
+
+function clearInp() {
+    var input = document.getElementById('in');
+    input.value = ""
+
+}
+
